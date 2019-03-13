900935a9b043cb54570b574df9bd2b5b6f1caf52
segunemma2003 <segunemma2003@gmail.com>
2019-03-13 14:47:43 +0100

    transaction

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.4/css/mdb.min.css" rel="stylesheet">
     <link href="{{ asset('css/sweetalert.css')}}"/>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.31.1/dist/sweetalert2.all.min.js"></script>
+    <script src="{{  asset('js/sweetalert.js')}}"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script> -->
     
 </head>
