2be280128334ce870c13f35f7fa391f770b41f79
segunemma2003 <segunemma2003@gmail.com>
2019-03-14 11:05:09 +0100

    transaction

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Events\TransactionEvent;
 
 Route::get('/pusher', function () {
     $message='hey';
-    \Alert::message('You are welcome to my site ');
+    \Alert::message('message','You are welcome to my site ');
     event(new TransactionEvent($message));
     return view('pusher');
 });
