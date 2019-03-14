821628a9c7f909bd49131c70381c5a22f5fac501
segunemma2003 <segunemma2003@gmail.com>
2019-03-14 12:48:55 +0100

    ttttt

--- resources/views/kwor-admin/index.blade.php
+++ resources/views/kwor-admin/index.blade.php
@@ -1,30 +1,5 @@
 @extends('layouts.user.master')
 @section('content')
-<div class="d-flex align-items-stretch">
-      <div id="sidebar" class="sidebar py-3">
-        <div class="text-black-400 text-uppercase px-3 px-lg-4 py-4 font-weight-bold small headings-font-family" style="color: green;">MY KWUO ACCOUNT</div>
-        <ul class="sidebar-menu list-unstyled">
-              <li class="sidebar-list-item"><a href="index.html" class="sidebar-link text-muted active"><i class="fa fa-gift mr-3 text-gray"></i><span>Dashboard</span></a></li>
-             <li class="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#pages" aria-expanded="false" aria-controls="pages" class="sidebar-link text-muted"><i class="fa fa-coins mr-3 text-gray"></i><span>My Account</span></a>
-                <div id="pages" class="collapse">
-                  <ul class="sidebar-menu list-unstyled border-left border-primary border-thick">
-                    <li class="sidebar-list-item"><a href="send-unit.html" class="sidebar-link text-muted pl-lg-5">Send Unit</a></li>
-                    <li class="sidebar-list-item"><a href="recieve-unit.html" class="sidebar-link text-muted pl-lg-5">Recieve Unit</a></li>
-                    <li class="sidebar-list-item"><a href="buy-unit.html" class="sidebar-link text-muted pl-lg-5">Buy Unit</a></li>
-                  </ul>
-                </div>
-              </li>
-               <li class="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#pages" aria-expanded="false" aria-controls="pages" class="sidebar-link text-muted"><i class="fa fa-user-circle mr-3 text-gray"></i><span>Merchant Account</span></a>
-                <div id="pages" class="collapse">
-                  <ul class="sidebar-menu list-unstyled border-left border-primary border-thick">
-                    <li class="sidebar-list-item"><a href="create-merch-acct.html" class="sidebar-link text-muted pl-lg-5">Create Merchant Account</a></li>
-                    <li class="sidebar-list-item"><a href="#" class="sidebar-link text-muted pl-lg-5">View Merchant Account</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li class="sidebar-list-item"><a href="transactions.html" class="sidebar-link text-muted"><i class="fa fa-history mr-3 text-gray"></i><span>Transactions</span></a></li>
-        </ul>
-      </div>
       <div class="page-holder w-100 d-flex flex-wrap">
         <div class="container-fluid px-xl-5">
           <section class="py-5">
