b63f15c3580bc4e13695750ce17613d1445fe5b6
segunemma2003 <segunemma2003@gmail.com>
2019-03-15 15:59:50 +0100

    transaction

--- resources/views/kwor-admin/buy_unit.blade.php
+++ resources/views/kwor-admin/buy_unit.blade.php
@@ -43,4 +43,22 @@
           </section>
         </div>
         
+        <script>
+      alert("hi");
+   function Naira(){
+	var unit= document.getElementById('unit').value;
+
+
+    document.getElementById('ans').innerHTML= 1 * unit;
+    return true;
+}
+function add(){
+  var units=document.getElementById("unit").value;
+  var new=document.getElementById('total');
+  new.value=units*1*100;
+  console.log(new.value);
+}	
+
+</script>
+        
 @endsection
