254eafbdf98b15d2e9413103eabc39b3a30f6eac
segunemma2003 <segunemma2003@gmail.com>
2019-03-19 01:46:02 +0000

    kkkkk

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -68,10 +68,10 @@ class TransactionController extends Controller
         }
 
     }
-    public function acceptRequest(Request $request)
+    public function acceptRequest(Request $request,$i)
     {
-        dd($request->all());
-        $transact=Transaction::whereTransaction_code($request->transaction_code)->first();
+        dd($i);
+        $transact=Transaction::whereTransaction_code($i)->first();
 
         $tt=Account::whereId($transact->receiver_id)->first();
         if($request->response==1 && ($tt->balance >= $transact->amount))
