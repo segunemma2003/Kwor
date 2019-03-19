3822d3ad18289197c205254423f7f1b8778ee6b7
segunemma2003 <segunemma2003@gmail.com>
2019-03-19 08:36:55 +0000

    kkkkk

--- resources/views/kwor-admin/receiver.blade.php
+++ resources/views/kwor-admin/receiver.blade.php
@@ -68,7 +68,7 @@
                   <div class="modal-body">
                     <form method="post">
                       @csrf
-                      <input type="hidden" name="transaction_code" id="newcodes">
+                      <input  name="transaction_code" id="newcodes">
                       <input type="hidden" name="response" value="1">
                       <input type="text" name="transfer_code" placeholder="input private key">
                     <input type="submit" onclick="Load()">
@@ -93,7 +93,7 @@
                   <div class="modal-body">
                   <form method="post">
                   @csrf
-                    <input type="hidden" name="response" value="2">
+                    <input name="response" value="2">
                     <input type="text" name="reject" placeholder="reason for reject">
                     <input type="hidden" name="transaction_code" id="newcode">
                   <input type="submit" onclick="Load()">
