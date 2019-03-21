d23d4b854d0884fc7c0421c701855b3b3bccc1c3
segunemma2003 <segunemma2003@gmail.com>
2019-03-21 13:57:55 +0100

    transaction

--- resources/views/kwor-admin/qrcode.blade.php
+++ resources/views/kwor-admin/qrcode.blade.php
@@ -11,11 +11,25 @@
                     </h3>
                   </div>
                   <div class="card-body">
+                <div id="qr">
                     {!! $qr !!}
+                </div>
+                  <button onclick="printInfo()">Print Qrcode</button>
                   </div>
                 </div>
               </div>
             </div>
           </section>
         </div>
+        <script>
+        function printInfo() {
+          var prtContent = document.getElementById("qr");
+          var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+          WinPrint.document.write(prtContent.innerHTML);
+          WinPrint.document.close();
+          WinPrint.focus();
+          WinPrint.print();
+          WinPrint.close();
+        }
+        </script>
 @endsection
