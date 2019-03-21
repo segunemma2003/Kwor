af44864fa7ddc6ab9c034af0ebc6a2c7e04f85ff
segunemma2003 <segunemma2003@gmail.com>
2019-03-21 18:05:31 +0100

    transaction

--- app/Account.php
+++ app/Account.php
@@ -9,6 +9,6 @@ class Account extends Model
     protected $guarded=['id'];
     public function user()
     {
-        return $this->belongsTo('App\Account','user_id');
+        return $this->belongsTo('App\User','user_id');
     }
 }
