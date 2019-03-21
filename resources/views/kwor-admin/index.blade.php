f9fd4fae2071fc9aa485cb3a28ca4157e19e3c6d
segunemma2003 <segunemma2003@gmail.com>
2019-03-21 15:56:56 +0100

    transaction

--- resources/views/kwor-admin/index.blade.php
+++ resources/views/kwor-admin/index.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid px-xl-5">
           <section class="py-5">
             <div class="row">
-              <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="col-xl-4 col-lg-6 mb-4 mb-xl-0">
                 <div class="coins shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
                   <div class="flex-grow-1 d-flex align-items-center">
                     <div class="dot mr-3 bg-violet"></div>
@@ -16,7 +16,7 @@
                   <div class="icon text-white bg-violet"><i class="fa fa-coins"></i></div>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="col-xl-4 col-lg-6 mb-4 mb-xl-0">
                 <div class="coins shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
                   <div class="flex-grow-1 d-flex align-items-center">
                     <div class="dot mr-3 bg-violet"></div>
@@ -27,7 +27,7 @@
                   <div class="icon text-white bg-violet"><i class="fas fa-server"></i></div>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-6 mb-4 mb-xl-0">
+              <div class="col-xl-4 col-lg-6 mb-4 mb-xl-0">
                 <div class="coins shadow roundy p-4 h-100 d-flex align-items-center justify-content-between">
                   <div class="flex-grow-1 d-flex align-items-center">
                     <div class="dot mr-3 bg-blue"></div>
