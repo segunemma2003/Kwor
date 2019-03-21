dffe9a68580a628aa5e9b4d2b63861cd13167b15
segunemma2003 <segunemma2003@gmail.com>
2019-03-21 17:06:04 +0100

    transaction

--- resources/views/kwor-admin/transact.blade.php
+++ resources/views/kwor-admin/transact.blade.php
@@ -80,24 +80,6 @@
                           <td>@mdo</td>
                           <td>@mdo</td>
                         </tr>
-                        <tr>
-                          <th scope="row">2</th>
-                          <td>Jacob</td>
-                          <td>Thornton</td>
-                          <td>@fat</td>
-                          <td>@mdo</td>
-                          <td>@mdo</td>
-                          <td>@mdo</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">3</th>
-                          <td>Larry</td>
-                          <td>the Bird</td>
-                          <td>@twitter</td>
-                          <td>@mdo</td>
-                          <td>@mdo</td>
-                          <td>@mdo</td>
-                        </tr>
                       </tbody>
                     </table>
                   </div>
