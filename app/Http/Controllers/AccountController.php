31a768b4d99892596a45431c0061a9902795e276
segunemma2003 <segunemma2003@gmail.com>
2019-03-28 18:02:56 +0100

    transaction~

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -134,11 +134,11 @@ class AccountController extends Controller
                     $transaction->save();
                     $tt=$tE->name;
                     $message=[
-                        'message'=>"{$tt} transferred {$request->amount} unit(s) to you. Your new account balance is {$Racc->balance}"
+                        'message'=>"{$tt} transferred {$request->amount} unit(s) to you for {$request->purpose}. Your new account balance is {$Racc->balance}"
                     ];
                     Mail::to($user->email)->send(new AlertMail($message));
                     $messages=[
-                        'message'=>"you just transferred {$request->amount} unit(s) to {$user->name}. Your new account balance is {$account->balance}"
+                        'message'=>"you just transferred {$request->amount} unit(s) to {$user->name} for {$request->purpose}. Your new account balance is {$account->balance}"
                     ];
                     Mail::to($tE->email)->send(new AlertMail($messages));
                     // $mess=Nexmo::message()->send([
