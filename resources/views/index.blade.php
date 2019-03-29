64f120004c68428d2b34694f0cd845a588ffacad
segunemma2003 <segunemma2003@gmail.com>
2019-03-29 15:25:24 +0100

    transaction~

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>kwuö</title>
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('img/core-img/favicon.ico') }}">
