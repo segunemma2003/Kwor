4e275366ec17b86ddb5bab898c4cc84e0d65d7b3
segunemma2003 <segunemma2003@gmail.com>
2019-03-29 15:59:04 +0100

    transaction~

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -280,49 +280,7 @@
     </section>
     <!-- ***** Cool Facts Area End ***** -->
 
-    <!-- ***** App Screenshots Area Start ***** -->
-    <section class="app-screenshots-area section_padding_0_100 clearfix" id="screenshot" style="background-color: #f0f0f0; margin-top: -50px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <!-- Heading Text  -->
-                    <div class="section-heading">
-                        <h2 style="color: #005502;" >App Screenshots</h2>
-                        <div class="line-shape"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <!-- App Screenshots Slides  -->
-                    <div class="app_screenshots_slides owl-carousel">
-                        <div class="single-shot">
-                            <img src="{{asset('kwor-admin/img/scr-img/app-1.jpg')}}" alt="">
-                        </div>
-                        <div class="single-shot">
-                            <img src="{{asset('kwor-admin/img/scr-img/app-2.jpg')}}" alt="">
-                        </div>
-                        <div class="single-shot">
-                            <img src="{{asset('kwor-admin/img/scr-img/app-3.jpg')}}" alt="">
-                        </div>
-                        <div class="single-shot">
-                            <img src="{{asset('kwor-admin/img/scr-img/app-4.jpg')}}" alt="">
-                        </div>
-                        <div class="single-shot">
-                            <img src="{{asset('kwor-admin/img/scr-img/app-5.jpg')}}" alt="">
-                        </div>
-                        <div class="single-shot">
-                            <img src="{{asset('kwor-admin/img/scr-img/app-3.jpg')}}" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- ***** App Screenshots Area End *****====== -->
-
+   
     
     <!-- ***** CTA Area Start ***** -->
     <section class="our-monthly-membership section_padding_50 clearfix" id="contact">
